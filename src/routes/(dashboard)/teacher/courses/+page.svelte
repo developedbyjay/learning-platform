002ffa9082963e7ec06c7b1decc6e561a7f5c15c
developedbyjay <okegbemijoshua7@gmail.com>
2025-02-07 18:56:18 +0100

    feat: add teacher/course route

---
+++ src/routes/(dashboard)/teacher/courses/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { PlusCircle } from 'lucide-svelte';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div class="p-6">
+	<div class="flex items-center py-4">
+		<Button href="/teacher/create" class="ml-auto">
+			<PlusCircle class="size-4" />
+			<span class="ml-4"> New Course </span>
+		</Button>
+	</div>
+</div>
---
+++ src/routes/(dashboard)/teacher/create/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	export var data:any;
+	
+	const form = superForm(data.form, {
+		validators: zodClient()
+	});
+</script>
+
+<div class="mx-auto flex h-full max-w-5xl border p-6 md:items-center md:justify-center">
+	<div>
+		<h1 class="text-2xl">Name Your Course</h1>
+		<p class="mt-3 text-sm text-muted-foreground">
+			What would you like to name your course? Don't worry, you can change it later
+		</p>
+		<form action="/teacher/create" method="POST" class="mt-8 space-y-8"></form>
+	</div>
+</div>
